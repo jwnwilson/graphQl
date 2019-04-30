e7731a8395746daa337d8dbe9024c65e619d6af6
Noel Wilson <noel.wilson@supercarers.com>
2019-04-30 19:10:12 +0100

    WIP looking at nested api requests

--- src/schema.py
+++ src/schema.py
@@ -24,17 +24,50 @@ def json2obj(data):
     return json.loads(json.dumps(data), object_hook=_json_object_hook)
 
 
-class CharacterSummary(ObjectType):
+class MarvelSummary(ObjectType):
     resourceURI = String()
     name = String()
     role = String()
 
 
-class CharacterList(ObjectType):
+class CharacterSummary(MarvelSummary):
+    pass
+
+
+class StorySummary(MarvelSummary):
+    pass
+
+
+class EventSummary(MarvelSummary):
+    pass
+
+
+class MarvelList(ObjectType):
     available = Int()
     returned = Int()
     collectionURI = String()
+
+
+class CharacterList(MarvelList):
     items = List(CharacterSummary)
+
+    def resolve_items(self, info):
+        import pdb;pdb.set_trace()
+        if self.items:
+            character_ids = [
+                c.collectionURI.split('/')[-1] for c in self.items
+            ]
+
+            characters = API.character.get_batch(character_ids)
+        return json2obj(characters)
+
+
+class StoryList(ObjectType):
+    items = List(StorySummary)
+
+
+class EventList(ObjectType):
+    items = List(EventSummary)
 
 
 class Comic(ObjectType):
@@ -46,15 +79,21 @@ class Comic(ObjectType):
     description = String()
     modified = String()
     characters = Field(CharacterList)
-
-    def resolve_comic(self, info):
-        return json2obj(API.comic.get(self.comic_id))
+    stories = Field(StoryList)
+    events = Field(EventList)
 
 
 class Query(ObjectType):
+    comic = Field(
+        Comic,
+        id=String(required=True)
+    )
     comics = List(
         Comic,
     )
+
+    def resolve_comic(self, info, id):
+        return json2obj(API.comic.get(id))
 
     def resolve_comics(self, info):
         return json2obj(API.comic.find())
